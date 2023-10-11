1dfa32529a831dda4e8480c2391d9c78314c0b7b
suhair <75605826+Suhairkm@users.noreply.github.com>
2023-10-11 10:54:25 +0530

    Update samplelinux.groovy
    s1d

--- vars/samplelinux.groovy
+++ vars/samplelinux.groovy
@@ -2,7 +2,7 @@ echo "The current working directory is : "
 sh "echo $pwd"
 echo "list of files are : "
 sh "echo $ls"
-def content(m) {
+def content(str m = "") {
   sh "the content of file {$m} are :"
   sh "echo $cat m" 
 }
