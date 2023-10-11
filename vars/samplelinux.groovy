257f47e3f99ba72dab060bfa0e71657e0ba0e31e
suhair <75605826+Suhairkm@users.noreply.github.com>
2023-10-11 11:11:58 +0530

    Update samplelinux.groovy
    s4d

--- vars/samplelinux.groovy
+++ vars/samplelinux.groovy
@@ -1,8 +1,11 @@
 def call() {
   echo "The current working directory is : "
   sh "echo $pwd"
-  echo "current username : "
-  sh "echo $whoami"
+def search(l) {
+  echo "search results for {$l} are :
+  sh "echo $find l"
+}
+  
 }
 def content(m) {
   sh "the content of file {$m} are :"
