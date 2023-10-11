f39c73a590ad02d3e22c7e4045b0c7da12fd3c58
suhair <75605826+Suhairkm@users.noreply.github.com>
2023-10-11 11:12:42 +0530

    Update samplelinux.groovy
    s5d

--- vars/samplelinux.groovy
+++ vars/samplelinux.groovy
@@ -2,7 +2,7 @@ def call() {
   echo "The current working directory is : "
   sh "echo $pwd"
 def search(l) {
-  echo "search results for {$l} are :
+  echo "search results for {$l} are :"
   sh "echo $find l"
 }
   
